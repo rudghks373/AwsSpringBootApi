c0c999de0b474c04944ae99c5d9acc39c175ae6b
rudghks373 <rudghks373@naver.com>
2020-01-08 21:34:18 +0900

    lombok add

--- src/test/java/springboot/web/HelloControllerTest.java
+++ src/test/java/springboot/web/HelloControllerTest.java
@@ -1,3 +1,5 @@
+package springboot.web;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
---
+++ src/test/java/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,23 @@
+package springboot.web.dto;
+
+import com.springboot.api.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void testlombok() {
+
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+
+    }
+}
