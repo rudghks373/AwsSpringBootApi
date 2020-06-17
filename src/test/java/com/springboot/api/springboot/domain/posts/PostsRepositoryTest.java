2b0ccba0de03479e10b4c7d0511900840f26ef4c
KyungHwan <rudghks373@naver.com>
2020-06-18 01:13:40 +0900

    Posts 테스트 케이스 추가

--- src/test/java/com/springboot/api/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/springboot/api/springboot/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PostsRepositoryTest {
@@ -24,6 +28,20 @@ public class PostsRepositoryTest {
         //given
         String title = "테스트 게시글";
         String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder().title(title)
+                                            .content(content)
+                                            .author("rudghks373@test.com")
+                                            .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
     }
 
 }
